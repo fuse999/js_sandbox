e26e0b523d5772ddf10b7a4d8936bb297d477de9
riley pence <rileypence4@gmail.com>
2020-05-21 13:36:36 -0700

    messing around with some js

---
+++ js_full_cfb/data_types.js
@@ -0,0 +1,11 @@
+/* Data Types
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myname = "Riley"
+myname = 8
+
+let ourname = "freecodecamp" // let will only work in a scope
+ourname = 9
+
+const pi = 3.14 // Will error out if you try to change
